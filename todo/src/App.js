28e3461dd341152eaa8de4d178ffc4697e4e9718
Shravan .V <perfectparameter24@gmail.com>
2022-02-09 23:09:03 +0530

    working on delete funciton

--- todo/src/App.js
+++ todo/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <h1>First To-Do</h1>
       </header>
       <Form inpText={inpText} todos={todos} settodos={settodos} setinpText={setinpText}/>
-      <TodoList/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
