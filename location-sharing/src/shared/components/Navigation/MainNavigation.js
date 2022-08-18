d19e80931796eee5d076923df8b501132d1f4eff
Shravan .V <perfectparameter24@gmail.com>
2022-08-18 14:03:44 +0530

    portal understandings

--- location-sharing/src/shared/components/Navigation/MainNavigation.js
+++ location-sharing/src/shared/components/Navigation/MainNavigation.js
@@ -8,27 +8,27 @@ import './MainNavigation.css';
 const MainNavigation = props => {
     return(
         <React.Fragment>
-        <SideDrawer>
-            <nav className="main-navigation__drawer-nav">
+            <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <Navlinks/>
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">
+                        YourPlaces
+                    </Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
                 <Navlinks/>
-            </nav>
-        </SideDrawer>
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/">
-                    YourPlaces
-                </Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-              <Navlinks/>
-            </nav>
-        </MainHeader>
+                </nav>
+            </MainHeader>
         </React.Fragment>
     );
-}
+};
 export default MainNavigation;
